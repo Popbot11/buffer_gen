80963e21858eca68daa91569eab14fed476a2e19
Robin Gillespie <legowoodtrashcan@gmail.com>
2024-12-11 19:21:12 -0500

    toml file read better scope memory usage

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 mod core;
 mod modules;
 use core::{module::Module, sample::Sample};
-use std::{cell::RefCell, collections::HashMap, rc::Rc};
-
+use std::{cell::RefCell, collections::HashMap, fs::File, io::Read, rc::Rc};
 use modules::{buffer::Buffer, gain::Gain, param::Param, render::Render};
-use text_io::read;
+use toml::Table;
 
 fn go(renderer: String, mdl_cache: &mut HashMap<String, Box<dyn Module>>, buff_cache: Rc<RefCell<HashMap<String, Vec<Sample>>>>) -> () {
     mdl_cache[&renderer].tick_sample(mdl_cache, 0); //i does nothing here
@@ -17,6 +16,14 @@ fn main() {
     let mut buffer_cache: Rc<RefCell<HashMap<String, Vec<Sample>>>> = Rc::new(RefCell::new(HashMap::new()));
     let mut module_cache: HashMap<String, Box<dyn Module>> = HashMap::new();
 
+    let toml_file = {
+        let mut file = File::open("test.toml").expect("Unable to open file");
+        let mut data = String::new();
+        file.read_to_string(&mut data).expect("Unable to read string");
+        println!("{}", data);
+        data.parse::<Table>().unwrap()
+    }; //I LOVE RUST YEAGAh
+    
     // names cannot contain spaces.
     module_cache.insert("renderer_module".to_string(), Render::new("create_buffer".to_string(), "audio".to_string(), buffer_cache.clone()));
     module_cache.insert("create_buffer".to_string(), Buffer::new("gain".to_string(), "audio".to_string(), 22050, buffer_cache.clone()));
